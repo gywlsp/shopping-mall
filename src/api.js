68a27fa010a18eb488cf64d86fb236c3fa8d0919
gywlsp <gywls517@gmail.com>
2022-05-16 07:44:02 +0900

    feat: :sparkles: request 함수 추가

---
+++ src/api.js
@@ -0,0 +1,12 @@
+const API_END_POINT =
+  "https://uikt6pohhh.execute-api.ap-northeast-2.amazonaws.com/dev";
+
+export const request = async (url) => {
+  const res = await fetch(url);
+
+  if (!res.ok) {
+    throw new Error("요청 실패");
+  }
+
+  return await res.json();
+};
